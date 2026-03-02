5c6b0a0e083a53240cd38f959f58a6111d4f0f7f
mbamaebd121-dotcom <mbamaebd121@gmail.com>
2026-03-02 22:19:19 +0300

    update design card

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,10 +1,10 @@
 function ProductCard({ image_url, name, description, price }) {
   return (
     <>
-      <div className="card product w-60 bg-white shadow rounded-b-2xl">
+      <div className="card product w-60 h-90 bg-white shadow rounded-b-2xl relative">
         <div className="header-card ">
           {image_url ? (
-            <img src={image_url} className="w-50 h-40 " />
+            <img src={image_url} className="w-60 h-40 " />
           ) : (
             <div className="img-virtual bg-gray-200 h-40"></div>
           )}
@@ -18,7 +18,7 @@ function ProductCard({ image_url, name, description, price }) {
             <p className="text-blue-600 font-bold">${price}</p>
           </div>
         </div>
-        <div className="footer-card border-t-2 border-gray-100 py-2 p-5 flex flex-col">
+        <div className="footer-card absolute left-0 right-0 bottom-0  border-t-2 border-gray-100 py-2 p-5 flex flex-col">
           <div className="container flex justify-between items-center pb-1">
                       <div className="container-star flex justify-start ">
             {Array(5)
