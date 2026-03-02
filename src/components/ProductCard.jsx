47126cacaeef0ced4e35ada53ce3ad0850c0d835
mbamaebd121-dotcom <mbamaebd121@gmail.com>
2026-03-02 22:44:50 +0300

    update design ProductCard

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -17,14 +17,16 @@ function ProductCard({ image_url, name, description, price,is_active }) {
             <div className="img-virtual bg-gray-200 h-40"></div>
           )}
         </div>
-        <div className="body-card flex justify-between  p-5">
+        <div className="body-card flex flex-col justify-between  p-5">
+          
           <div className="info ">
             <h1 className="font-bold text-lg">{name}</h1>
-            <p className="text-gray-600 text-sm">{description}</p>
+            
           </div>
           <div className="price flex items-center">
             <p className="text-blue-600 font-bold">${price}</p>
           </div>
+          <p className="text-gray-600 text-sm">{description}</p>
         </div>
         <div className="footer-card absolute left-0 right-0 bottom-0  border-t-2 border-gray-100 py-2 p-5 flex flex-col">
           <div className="container flex justify-between items-center pb-1">
