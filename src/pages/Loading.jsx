562f0d82cdc3a003077f8a535825e98430eb7ba5
mbamaebd121-dotcom <mbamaebd121@gmail.com>
2026-03-05 22:35:02 +0300

    Update Loading.jsx
    update loadin.jsx

--- src/pages/Loading.jsx
+++ src/pages/Loading.jsx
@@ -3,13 +3,13 @@ function Loading() {
   return (
     <div class="loading-screen ">
 
-          {/* <div className="spinner"></div>
-      <p>جاري التحميل...</p> */}
-  <div className="flex flex-wrap gap-2">
+          <div className="spinner"></div>
+      <p>جاري التحميل...</p>
+  {/* <div className="flex flex-wrap gap-2">
     {Array(4).fill().map((_, i) => (
       <div key={i} className="w-50 h-60 bg-gray-200 animate-pulse rounded-xl"></div>
     ))}
-  </div>
+  </div> */}
     </div>
   );
 }
